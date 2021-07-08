810114fd5380f0dd0d6a55359e18dfaaedc6c379
Mike <maquinde@users.noreply.github.com>
2021-07-07 20:40:44 -0700

    completed ex5

---
+++ ex/ex5.py
@@ -0,0 +1,20 @@
+name = 'Zed A. Shaw'
+age = 35
+height = 74
+cm = 2.54 * height
+weight = 180
+kg = weight / 2.205
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+print(f"Let's talk aboutn {name}.")
+print(f"He's {height} inches or {cm} centimeters tall.")
+print(f"He's {weight} pounds or {kg} kilograms heavy.")
+print("Actually that's not too heavy")
+print(f"He's got {eyes} eyes and {hair} hair.")
+print(f"His teeth are usually {teeth} depending on the coffee.")
+
+total = age + height + weight
+print(f"If I add {age}, {height}, and {weight} I get {total}.")
+
