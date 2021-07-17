34f8fb52ce6e6097669f764f59cb0269db4c16d6
Mike <maquinde@users.noreply.github.com>
2021-07-17 13:02:20 -0700

    completed ex8

---
+++ ex/ex8.py
@@ -0,0 +1,22 @@
+#assign 4 curly braces to variable
+formatter = "{} {} {} {}"
+
+#prints variable and injects 1234 with .format() function
+print(formatter.format(1,2,3,4))
+
+#prints variable and injects strings with .format() function
+print(formatter.format("one", "two", "three", "four"))
+
+#prints variable and injects bools with .format() function
+print(formatter.format(True, False, False, True))
+
+#prints variable and injects same variable with .format() function
+print(formatter.format(formatter, formatter, formatter, formatter))
+
+#prints variable and injects custom string with .format() function
+print(formatter.format(
+    "Hello", 
+    "there.", 
+    "I'm", 
+    "Mike"
+))
