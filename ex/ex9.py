afb4c2919d20e99967c39dc56ea5f5bcbb2302a4
Mike <maquinde@users.noreply.github.com>
2021-07-19 20:46:27 -0700

    completed ex9

---
+++ ex/ex9.py
@@ -0,0 +1,19 @@
+#Here's some new strange stuff, remember type it exactly.
+
+#assign string of days to variable
+days = "Mon Tue Wed Thu Fri Sat Sun"
+
+#assign months to variable with newline after each month
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+#prints string, then the variables we set
+print("Here are the days: ", days)
+print("Here are the months ", months)
+
+#prints multiline string using 6 double-quotes
+print("""
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+""")
