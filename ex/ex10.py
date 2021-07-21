13e0e68240a3a3008975804b2cc3e9fc6c32efef
Mike <maquinde@users.noreply.github.com>
2021-07-20 22:58:04 -0700

    finished colt steele's code along

---
+++ ex/ex10.py
@@ -0,0 +1,9 @@
+#Colt Steele - Mileage Converter
+
+# print("How many kilometers did you run today? ")
+# kilometers = float(input())
+# miles = kilometers / 1.6
+# miles = round(miles, 2)
+# print(f"Your {kilometers}km run was {miles}mi")
+
+#round(what to round, by how many decimal points)
