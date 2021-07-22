1c3ecb55ceadb91980390689570b74f2155d4105
Mike <maquinde@users.noreply.github.com>
2021-07-21 19:44:40 -0700

    completed ex10

--- ex/ex10.py
+++ ex/ex10.py
@@ -1,9 +1,39 @@
 #Colt Steele - Mileage Converter
 
+#PRINTS QUESTION TO USER
 # print("How many kilometers did you run today? ")
+
+#PROMPTS USER FOR AN ANSWER, CONVERTS THE ANSWER INTO A FLOAT, THEN ASSIGNS TO VARIABLE
 # kilometers = float(input())
+
+#TAKES USER INPUT AND DIVIDES BY 1.6 AND ASSIGNS TO VARIABLE
 # miles = kilometers / 1.6
+
+#USES THE ROUND FUNCTION AND ADDS THE VARIABLE AND HOW MANY DECIMAL PLACES TO ROUND. ASSIGNS OUTPUT TO VARIABLE
 # miles = round(miles, 2)
+
+#PRINTS USERS INPUT USING F-STRING
 # print(f"Your {kilometers}km run was {miles}mi")
 
 #round(what to round, by how many decimal points)
+
+
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
+
+print("Backslash \\")
+print("single quote \'")
+print("double-quote \"")
