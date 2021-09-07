999912e9c0544b9bed59b4b343c4a25d5756beba
Mike <maquinde@users.noreply.github.com>
2021-09-07 13:10:03 -0700

    completed ex11

---
+++ ex/ex11.py
@@ -0,0 +1,26 @@
+print("How old are you?", end=" ")
+age = input()
+print("How tall are you?", end=" ")
+height = input()
+print("How much do you weigh?", end=" ")
+weight = input()
+
+print(f"So, you're {age} years old, {height} tall and {weight} heavy.")
+
+
+#Homework
+# Go online and find out what Python's input does.
+# input() takes input and converts into string
+
+# Can you find other ways to use it? Try some of the samples you find.
+name = input("Enter your name: ")
+print("Hello ", name)
+
+# Write another "form" like this to ask some other questions.
+
+print("Where are you located?", end=" ")
+location = input()
+
+last_name = input("What is your last name?")
+
+print(f"Your last name is {last_name} and you are from {location}.")
