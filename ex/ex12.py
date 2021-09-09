c0082462dd1ad9f0a681d98579451f443d78f4fa
Mike <maquinde@users.noreply.github.com>
2021-09-09 06:32:50 -0700

    added some notes for the homework

--- ex/ex12.py
+++ ex/ex12.py
@@ -3,3 +3,9 @@ height = input("How tall are you? ")
 weight = input("How much do you weigh? ")
 
 print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+
+#Notes
+#open() - opens files
+#file() - opens files
+#os - manage os you are on
+#sys - ?
