d5d4cfb83e5e137838053edaccd17f2f95fb6307
Mike <maquinde@users.noreply.github.com>
2021-09-09 17:11:42 -0700

    added a test file

---
+++ ex/ex13.py
@@ -0,0 +1,11 @@
+from sys import argv
+#read the WYSS section for how to run this 
+script, first, second, third = argv
+
+print("The script is called:", script)
+print("Your first variable is:", first)
+print("Your second variable is:", second)
+print("Your third variable is:", third)
+
+#run using cmd with arguments (python ex13.py arg1 arg2 arg3)
+
---
+++ ex/test.py
@@ -0,0 +1,11 @@
+from sys import argv
+
+#Homework
+one, two, three, four = argv
+
+name = input("Type something...")
+print("This is what you typed:", name)
+print("Your arguments are:", one)
+print("and:", two)
+print(three)
+print(four)
