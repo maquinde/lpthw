52ecb927a41bb818e4a99b9a7b99f3eae2db8ef8
Mike <maquinde@users.noreply.github.com>
2021-09-13 16:13:01 -0700

    completed ex14

--- ex/test.py
+++ ex/test.py
@@ -1,11 +1,22 @@
 from sys import argv
-#read the WYSS section for how to run this 
-script, first, second, third = argv
 
-print("The script is called:", script)
-print("Your first variable is:", first)
-print("Your second variable is:", second)
-print("Your third variable is:", third)
+script, user_name, password = argv
+prompt = ">>> "
 
-answer = input("What is your name?")
-print("Your name is:", answer)
+print(f"Hi {user_name}, I'm the {script} script.")
+print("I'd like to ask you a few questions.")
+print(f"Do you like me {user_name}?")
+likes = input(prompt)
+
+print(f"Where do you live {user_name}?")
+lives = input(prompt)
+
+print("What kind of computer do you have?")
+computer = input(prompt)
+
+print(f"""
+Alright, so you said {likes} about liking me.
+You live in {lives}. Not sure where that is.
+And you have a {computer} computer. Nice.
+Your password is {password}...
+""")
