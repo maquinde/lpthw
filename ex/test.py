ead9a69aac63f6bf2e0b81f10dd5f5ecd8e2e864
Mike <maquinde@users.noreply.github.com>
2021-09-15 17:07:35 -0700

    completed ex16

--- ex/test.py
+++ ex/test.py
@@ -2,14 +2,8 @@ from sys import argv
 
 script, filename = argv
 
-txt = open(filename)
+file = open(filename, "w")
 
-print(f"Here's your file {filename}:")
-print(txt.read())
-
-print("Type the filename again:")
-file_again = input("> ")
-
-txt_again = open(file_again)
-
-print(txt_again.read())
+file.read()
+file.write("This is a test")
+file.close()
